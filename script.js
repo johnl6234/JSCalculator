edcda96cff6b1f9b812093dce5856af2548c3ecc
John Lockham <47853652+johnl6234@users.noreply.github.com>
2020-05-20 17:41:39 +0100

    Added "C" button and functionality

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.number-button').forEach(button => {
         button.onclick = () => {
-            if (button.innerHTML === "=") {
+            if (button.innerHTML === "C") {
+                if (secondNumbers.length > 0) {
+                    secondNumbers = "";
+                    document.getElementById('screen-text').innerHTML = "";
+                } else if (multiplier !== "") {
+                    multiplier = "";
+                    document.getElementById('small-screen-text').innerHTML = firstNumbers;
+                } else if (firstNumbers.length > 0) {
+                    firstNumbers = "";
+                    document.getElementById('screen-text').innerHTML = "";
+                }
+            } else if (button.innerHTML === "=") {
                 {
                     if (firstNumbers.length > 0 && secondNumbers.length > 0 && multiplier.length > 0) {
                         var firstFloat = parseFloat(firstNumbers);
@@ -27,22 +38,22 @@ document.addEventListener('DOMContentLoaded', () => {
                                 break;
                         }
                         document.getElementById('small-screen-text').innerHTML = firstNumbers + " " + multiplier + " " + secondNumbers;
-
+                        if (answer.length > 7) {
+                            document.getElementById('screen-text').setAttribute('class', 'screen-text-small');
+                        }
                         document.getElementById('screen-text').innerHTML = answer;
                         multiplier = "";
                         firstNumbers = "";
                         secondNumbers = "";
                     }
                 }
-            }else
-            if (button.innerHTML === "+" ||
+            } else if (button.innerHTML === "+" ||
                 button.innerHTML === "-" ||
                 button.innerHTML === "*" ||
                 button.innerHTML === "/" && firstNumbers.length > 0) {
                 if (multiplier === "") {
                     multiplier = button.innerHTML;
                     document.getElementById('small-screen-text').innerHTML = firstNumbers + " " + multiplier;
-
                 } else {
                     if (firstNumbers.length > 0 && secondNumbers.length > 0 && multiplier.length > 0) {
                         var firstFloat = parseFloat(firstNumbers);
@@ -70,9 +81,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         secondNumbers = "";
                     }
                 }
-
-             } else if (multiplier === "") {
-                if (firstNumbers.length < 14) {
+            } else if (multiplier === "") {
+                if (firstNumbers.length < 12) {
                     firstNumbers += button.innerHTML;
                 };
                 if (firstNumbers.length > 7) {
@@ -80,8 +90,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 };
 
                 document.getElementById('screen-text').innerHTML = firstNumbers;
-            } else {
-                if (secondNumbers.length < 14) {
+            } else if (button.innerHTML != "C") {
+                if (secondNumbers.length < 12) {
                     secondNumbers += button.innerHTML;
                 };
                 if (secondNumbers.length > 7) {
@@ -89,9 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 };
 
                 document.getElementById('screen-text').innerHTML = secondNumbers;
-
             }
         };
     });
-
 })
